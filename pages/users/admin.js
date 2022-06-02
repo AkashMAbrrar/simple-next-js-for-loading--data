2dd5d85d4e1b842c19dbbc5f07360332c8041e45
AkashAbrrar <mabrrar9@gmail.com>
2022-06-02 14:06:55 +0600

    this is the commit for my first simple next js project

---
+++ pages/users/admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const admin = () => {
+    return (
+        <div>
+            <h2>This Is Honorable Admin</h2>
+        </div>
+    );
+};
+
+export default admin;
---
+++ pages/users/managers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const managers = () => {
+    return (
+        <div>
+            <h2>All Managers Are Here For Going To Jail</h2>
+        </div>
+    );
+};
+
+export default managers;
